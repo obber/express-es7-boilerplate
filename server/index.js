ebd87e7ab2d3e64cc96c1b3b1018ded55e8b0e56
Kan Adachi <Kadachi1023@gmail.com>
2016-09-24 12:43:09 -0700

    [setup] added express listen

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
-console.log('hello!');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT);
+console.log(`express server listening on port ${3000}`);
